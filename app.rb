35220ce7884d9812d75f3b5ae3d3314bba183ada
4myc <missch3n@gmail.com>
2019-12-07 22:03:18 -0800

    done

--- app.rb
+++ app.rb
@@ -1,7 +1,8 @@
-class Application < Sinatra::Base
+#A Sinatra Controller is a Ruby Class that inherits from Sinatra::Base
+class Application < Sinatra::Base #inherit from Sinatra so any instance of the class App will have functionality of Sinatra
 
-  get '/' do
-    "Hello, World!"
+  get '/' do #method responds to a GET request to the root url
+    "Hello, World!" #display "Hello, world!" in the browser
   end
 
 end
--- config.ru
+++ config.ru
@@ -1,5 +1,5 @@
-require 'sinatra'
+require 'sinatra' #load the Sinatra library
 
-require_relative './app.rb'
+require_relative './app.rb' #requires our application file, defined in app.rb
 
-run Application
+run Application #uses run to start the application represented by the ruby class Application, which is defined in app.rb
